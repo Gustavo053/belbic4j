3e9a3b0d564db22a6c3c6244759048120d88de44
gustavo <gugupf10@gmail.com>
2022-05-28 17:55:12 -0300

    [#4] Improving library architecture

---
+++ src/main/java/equation/BelbicEquation.java
@@ -0,0 +1,30 @@
+package equation;
+
+public class BelbicEquation {
+
+    public static Double weightAmygdalaEquation(
+            Double alpha,
+            Double sensoryInputValue,
+            Double rew,
+            Double amygdalaValue
+    ) {
+        return alpha * (sensoryInputValue * Math.max(0, rew - amygdalaValue));
+    }
+
+    public static Double weightOrbitofrontalEquation(
+            Double beta,
+            Double sensoryInputValue,
+            Double rew,
+            Double orbitofrontalCortexValue
+    ) {
+        return beta * (sensoryInputValue * (orbitofrontalCortexValue - rew));
+    }
+
+    public static Double amygdalaEquation(Double weightAmygdala, Double sensoryInputValue) {
+        return weightAmygdala * sensoryInputValue;
+    }
+
+    public static Double orbitofrontalEquation(Double weightOrbitofrontal, Double sensoryInputValue) {
+        return weightOrbitofrontal * sensoryInputValue;
+    }
+}

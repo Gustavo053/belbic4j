e2bbcf6d692271c05c5c3c5fe2130c5a0d506e4a
gustavo <gugupf10@gmail.com>
2022-05-28 14:16:59 -0300

    [#1] Creating equations that will be called in the belbic blocks

--- src/main/java/contract/BelEquation.java
+++ src/main/java/contract/BelEquation.java
@@ -1,19 +1,36 @@
 package contract;
 
 public interface BelEquation {
-    Double thalamusExecuteEquation(Double sensoryInputValue);
-    Double weightAmygdalaExecuteEquation(
+
+    /*
+    The thalamus will not be used because the algorithm is working with a single emotional signal,
+    not an array of emotional signals
+    default Double thalamusExecuteEquation(Double sensoryInputValue) {
+
+    }
+     */
+
+    default Double weightAmygdalaEquation(
             Double alpha,
             Double sensoryInputValue,
             Double rew,
-            Double amygdalaCortexValue
-    );
-    Double weightOrbitofrontalExecuteEquation(
+            Double amygdalaValue
+    ) {
+        return alpha * (sensoryInputValue * Math.max(0, rew - amygdalaValue));
+    }
+    default Double weightOrbitofrontalEquation(
             Double beta,
             Double sensoryInputValue,
             Double rew,
             Double orbitofrontalCortexValue
-    );
-    Double amygdalaEquation(Double weightAmygdala, Double sensoryInputValue);
-    Double orbitofrontalEquation(Double weightOrbitofrontal, Double sensoryInputValue);
+    ) {
+        return beta * (sensoryInputValue * (orbitofrontalCortexValue - rew));
+    }
+    default Double amygdalaEquation(Double weightAmygdala, Double sensoryInputValue) {
+        return weightAmygdala * sensoryInputValue;
+    }
+
+    default Double orbitofrontalEquation(Double weightOrbitofrontal, Double sensoryInputValue) {
+        return weightOrbitofrontal * sensoryInputValue;
+    }
 }

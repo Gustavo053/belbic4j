6ca144e2330880f010f325a343ce10c42788c71e
gustavo <gugupf10@gmail.com>
2022-05-28 14:48:24 -0300

    [#2] Implementing belbic block logic

--- src/main/java/Belbic.java
+++ src/main/java/Belbic.java
@@ -5,62 +5,66 @@ import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.function.Consumer;
 
 @Getter
 @Setter
 public class Belbic implements Bel, BelEquation, Serializable {
+    private Double alpha;
+    private Double beta;
     private Double sensoryInputValue;
-    private Double amygdalaCortexValue;
-    private Double amygdalaCortexFinalValue;
+    private Double amygdalaValue;
     private Double orbitofrontalCortexValue;
-    private Double orbitofrontalCortexFinalValue;
     private Double weightAmygdala;
     private Double weightOrbitofrontal;
     private Double rewValue;
+
+    public void initParamsBelbic(Double alpha, Double beta, Double rewValue, Double weightAmygdala, Double weightOrbitofrontal) {
+        this.alpha = alpha;
+        this.beta = beta;
+        this.rewValue = rewValue;
+        this.weightAmygdala = weightAmygdala;
+        this.weightOrbitofrontal = weightOrbitofrontal;
+    }
 
     public void sensoryInputExecute(Runnable runnable){
         runnable.run();
     }
 
-    public Map thalamusExecute(Double sensoryInputValue) {
-        return null;
-    }
-
-    public Double sensoryCortexExecute(Double sensoryInputValue, Double rew, Double amygdalaCortexValue, Double amygdalaCortexFinalValue, Double weightAmygdala, Double weightOrbitofrontal) {
-        return null;
-    }
-
-    public Double orbitofrontalCortexExecute(Double weightOrbitofrontal, Double sensoryInputValue, Double amygdalaCortexValue, Double orbitofrontalCortexValue, Double rew) {
-        return null;
-    }
-
-    public Double amygdala(Double weightAmygdala, Double sensoryInputValue, Double amygdalaCortexValue, Double orbitofrontalCortexValue, Double rew) {
-        return null;
-    }
-
-    public Double thalamusExecuteEquation(Double sensoryInputValue) {
-        return null;
-    }
-
-    public Double weightAmygdalaExecuteEquation(Double alpha, Double sensoryInputValue, Double rew, Double amygdalaCortexValue) {
-        return null;
-    }
-
-    public Double weightOrbitofrontalExecuteEquation(Double beta, Double sensoryInputValue, Double rew, Double orbitofrontalCortexValue) {
-        return null;
-    }
-
-    public Double amygdalaEquation(Double weightAmygdala, Double sensoryInputValue) {
-        return null;
-    }
-
-    public Double orbitofrontalEquation(Double weightOrbitofrontal, Double sensoryInputValue) {
-        return null;
-    }
-
     public void rewExecute(Runnable runnable) {
         runnable.run();
+    }
+
+    public Double run() {
+        Map<String, Double> resultSensoryCortex = sensoryCortex(
+                alpha,
+                beta,
+                sensoryInputValue,
+                rewValue,
+                amygdalaValue,
+                orbitofrontalCortexValue,
+                weightAmygdala,
+                weightOrbitofrontal
+        );
+
+        this.weightAmygdala = resultSensoryCortex.get("newWeightAmygdala");
+        this.weightOrbitofrontal = resultSensoryCortex.get("newWeightOrbitofrontal");
+
+        Double resultOrbitofrontalCortex = orbitofrontalCortex(
+                weightOrbitofrontal,
+                sensoryInputValue,
+                rewValue
+        );
+
+        this.orbitofrontalCortexValue = resultOrbitofrontalCortex;
+
+        Double resultAmygdala = amygdala(
+                weightAmygdala,
+                sensoryInputValue,
+                rewValue
+        );
+
+        this.amygdalaValue = resultAmygdala;
+
+        return (amygdalaValue - orbitofrontalCortexValue) * rewValue;
     }
 }
--- src/main/java/contract/Bel.java
+++ src/main/java/contract/Bel.java
@@ -1,32 +1,47 @@
 package contract;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 
-public interface Bel {
+public interface Bel extends BelEquation {
     void sensoryInputExecute(Runnable runnable);
-    Map thalamusExecute(Double sensoryInputValue);
-    Double sensoryCortexExecute(
+//    Map thalamusExecute(Double sensoryInputValue);
+    default Map<String, Double> sensoryCortex(
+                Double alpha,
+                Double beta,
                 Double sensoryInputValue,
                 Double rew,
-                Double amygdalaCortexValue,
-                Double amygdalaCortexFinalValue,
+                Double amygdalaValue,
+                Double orbitofrontalCortexValue,
                 Double weightAmygdala,
                 Double weightOrbitofrontal
-            );
-    Double orbitofrontalCortexExecute(
+            ) {
+        Map<String, Double> result = new HashMap();
+        result.put(
+                "newWeightAmygdala",
+                weightAmygdalaEquation(alpha, sensoryInputValue, rew, amygdalaValue) + weightAmygdala
+        );
+        result.put(
+                "newWeightOrbitofrontal",
+                weightOrbitofrontalEquation(beta, sensoryInputValue, rew, orbitofrontalCortexValue) + weightOrbitofrontal
+        );
+
+        return result;
+    }
+    default Double orbitofrontalCortex(
             Double weightOrbitofrontal,
             Double sensoryInputValue,
-            Double amygdalaCortexValue,
-            Double orbitofrontalCortexValue,
             Double rew
-    );
-    Double amygdala(
+    ) {
+        return orbitofrontalEquation(weightOrbitofrontal, sensoryInputValue) * rew;
+    }
+     default Double amygdala(
             Double weightAmygdala,
             Double sensoryInputValue,
-            Double amygdalaCortexValue,
-            Double orbitofrontalCortexValue,
             Double rew
-    );
+    ) {
+        return amygdalaEquation(weightAmygdala, sensoryInputValue) * rew;
+     }
 }
